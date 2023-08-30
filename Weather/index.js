2cbd352335a7fad55f8f51df103ab25d7eb3fbf3
shree-77 <shreedhoni7@gmail.com>
2023-08-30 19:03:25 +0530

    UI created

--- Weather/index.js
+++ Weather/index.js
@@ -1,6 +1,10 @@
 const apiKey = 'lDTXnCLt9yBYkKiRzl8UmbEazHYn4ALD';
 const city = 'Vellore';
-
+const output = document.querySelector('.Output');
+const min = document.createElement('div');
+min.classList.add('Min');
+const max = document.createElement('div');
+max.classList.add('Max');
 
 
 
@@ -14,8 +18,12 @@ async function getLocation() {
 async function getWeather(CityKey) {
     const response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${CityKey}?apikey=${apiKey}`, { mode: 'cors' });
     const weather = await response.json();
-    console.log(weather);
+    min.textContent = `${weather["DailyForecasts"][0]["Temperature"]["Minimum"]['Value']}`;
+    max.textContent = `${weather["DailyForecasts"][0]["Temperature"]["Maximum"]['Value']}`;
+
 }
 
 getLocation();
 
+output.appendChild(min);
+output.appendChild(max);
