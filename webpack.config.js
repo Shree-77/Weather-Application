126295edbbcbcd4ebbc5047fa1daa66171e8af12
shree-77 <shreedhoni7@gmail.com>
2023-10-26 22:22:24 +0530

    git pages deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',  
     path: path.resolve(__dirname, 'dist'),  
+    publicPath: "/Weather-Application/",
   },
   module: {  // This section should be inside the main configuration object
     rules: [
