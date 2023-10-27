1fcbe4c1bf4323f56d844ddb50722bf4474f3cb5
shree-77 <shreedhoni7@gmail.com>
2023-10-27 17:26:33 +0530

    homepage,API Updated

--- Weather/index.js
+++ Weather/index.js
@@ -1,18 +1,25 @@
 const API_KEY = '4b01ba555e2f402cbbb35347232908';
-const API_URL= 'https://api.weatherapi.com/v1/forecast.json?'
+const API_URL = 'https://api.weatherapi.com/v1/forecast.json?';
 
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const weatherInfo = document.getElementById('weatherInfo');
 
 async function getWeather(location = 'valletta', days = '3') {
     const response = await fetch(API_URL + 'key=' + API_KEY + '&q=' + location + '&days=' + days + '&aqi=no&alerts=no', {
         mode: 'cors'
     });
     const data = await response.json();
-
-     console.log(data);
-
+    console.log(data);
+    const weatherData = `
+        <p>Location: ${data.location.name}, ${data.location.country}</p>
+        <p>Temperature: ${data.current.temp_c}°C</p>
+        <p>Condition: ${data.current.condition.text}</p>
+    `;
+    weatherInfo.innerHTML = weatherData;
 }
-getWeather();
 
-
-
-
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    getWeather(location);
+});
