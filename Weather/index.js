7d69c8d0752884c2399c24aeccc3356aee990892
shree-77 <shreedhoni7@gmail.com>
2023-10-27 18:24:57 +0530

    day/night bg added

--- Weather/index.js
+++ Weather/index.js
@@ -5,21 +5,40 @@ const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
 const weatherInfo = document.getElementById('weatherInfo');
 
-async function getWeather(location = 'valletta', days = '3') {
+function isDay(is_day, body) {
+    body.style.backgroundImage = 'none'; 
+    if (is_day) {
+        const dayBackgroundURL = './static/images/day.svg';
+         body.style.backgroundImage = `url(${dayBackgroundURL})`;
+    } else {
+        const nightBackgroundURL = './static/images/night.svg';
+        body.style.backgroundImage = `url(${nightBackgroundURL})`;
+    }
+}
+
+async function getWeather(location, days = '3') {
     const response = await fetch(API_URL + 'key=' + API_KEY + '&q=' + location + '&days=' + days + '&aqi=no&alerts=no', {
         mode: 'cors'
     });
     const data = await response.json();
+    if (data.error) {
+        alert('City not found');
+        return; 
+    }
     console.log(data);
+    const is_Day = data.current.is_day === 1;
     const weatherData = `
         <p>Location: ${data.location.name}, ${data.location.country}</p>
-        <p>Temperature: ${data.current.temp_c}°C</p>
+        <p>Celsius: ${data.current.temp_c}°C</p>
+        <p>Farenheit : ${data.current.temp_f}°F</p>
         <p>Condition: ${data.current.condition.text}</p>
     `;
-    weatherInfo.innerHTML = weatherData;
-}
 
+    weatherInfo.innerHTML = weatherData;
+    isDay(is_Day, document.body);
+}
 searchButton.addEventListener('click', () => {
     const location = locationInput.value;
     getWeather(location);
 });
+
